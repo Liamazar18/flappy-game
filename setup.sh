0901233673b54356cdc95f5a42561d0c4c56050f
Liamazar18 <liamazar18@gmail.com>
2025-05-29 13:45:33 -0400

    new second commit

--- setup.sh
+++
@@ -1,7 +0,0 @@
-echo "# flappy-game" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Liamazar18/flappy-game.git
-git push -u origin main
